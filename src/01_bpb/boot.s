4aec932f57126615b4348dd64f3e723c98fefa26
kino-ma <kino.ma.ms@gmail.com>
2020-08-17 17:08:26 +0900

    01_bpb - do nothing

---
+++ src/01_bpb/boot.s
@@ -0,0 +1,12 @@
+entry:
+	jmp		ipl
+
+	; BPB (BIOS Parameter Block)
+	times	90 - ($ - $$) db 0x90
+
+; IPL (Initial Program Loader)
+ipl:
+	jmp		$ ; do nothing
+
+	times	510 - ($ - $$) db 0x00
+	db		0x55, 0xAA
