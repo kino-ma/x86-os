741a0e88a08e1fa3985910cf025869dca0e63b81
kino-ma <kino.ma.ms@gmail.com>
2020-11-07 18:00:29 +0900

    remove stage2 on boot.s

--- src_asm/boot.s
+++ src_asm/boot.s
@@ -71,14 +71,14 @@ BOOT:             ; ブートドライブに関する情報
 
 ; 512 ~
 ; stage_2
-stage_2:
-    cdecl   puts, stage2_str
-
-    jmp     $   ; while (1);
-
-
-    ;times   (1024 * 8) - ($ - $$) db 0
-
-
-stage2_str  db "this is stage 2", 0x0a, 0x0d, 0
-    times   (1024 * 8) - ($ - $$) db 0
+;stage_2:
+;    cdecl   puts, stage2_str
+;
+;    jmp     $   ; while (1);
+;
+;
+;    ;times   (1024 * 8) - ($ - $$) db 0
+;
+;
+;stage2_str  db "this is stage 2", 0x0a, 0x0d, 0
+;    times   (1024 * 8) - ($ - $$) db 0
